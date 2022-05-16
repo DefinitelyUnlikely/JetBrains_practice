229c637a2845a72b658fb35c5569c6720b09ec8f
DefinitelyUnlikely <76999022+DefinitelyUnlikely@users.noreply.github.com>
2022-05-16 19:22:06 +0200

    Update regression_modelling.py

--- regression_modelling.py
+++ regression_modelling.py
@@ -1,5 +1,8 @@
 import pandas
 import numpy as np
+import sklearn.metrics
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
 
 
 class CustomLinearRegression:
@@ -7,16 +10,18 @@ class CustomLinearRegression:
     Class implement model fitting for a given dataset or predict outcome based on a model.
     """
 
-    def __init__(self, dataframe, fit_intercept=True):
+    def __init__(self, dataframe, fit_intercept=True, transpose=True):
         """
 
         @param dataframe: a pandas.Dataframe. y-values (dependent variable) is last row. X-matrix (independent values)
          are first rows.
         @param fit_intercept: specify if intercept is part of modelling or not. default is True.
+        @param transpose: If dataframe has variable data in rows, keep default. If y in separate column, set as False.
         """
         self.fit_intercept = fit_intercept
 
-        dataframe = dataframe.transpose()
+        if transpose:
+            dataframe = dataframe.transpose()
         if fit_intercept:
             dataframe.insert(0, "ones", 1)
         self.X = np.array(dataframe.iloc[:, :-1])
@@ -26,6 +31,8 @@ class CustomLinearRegression:
         self.intercept = None
         self.coefficient = None
         self.y_hat = None
+        self.r2_score = None
+        self.rmse_score = None
 
     def fit(self):
         xt_at_x_inverse = np.linalg.inv(self.X.transpose() @ self.X)
@@ -35,12 +42,20 @@ class CustomLinearRegression:
         self.intercept, *self.coefficient = self.beta
 
     def predict(self):
-        if not self.beta:
+        if self.beta is None:
             self.fit()
         self.y_hat = self.X @ self.beta
 
-    def r2_score(self):
-        pass
+    def r2(self):
+        if self.y_hat is None:
+            self.predict()
+
+        nominator = sum([(i - j) ** 2 for i, j in zip(self.Y, self.y_hat)])
+        denominator = sum([(i - self.Y.mean()) ** 2 for i in self.Y])
+
+        self.r2_score: float = 1 - (nominator / denominator)
 
     def rmse(self):
-        pass
+        if self.y_hat is None:
+            self.predict()
+        self.rmse_score = (sum([(i - j) ** 2 for i, j in zip(self.Y, self.y_hat)]) / len(self.Y)) ** 0.5
